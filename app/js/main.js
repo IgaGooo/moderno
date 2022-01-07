52bceed68e4e51974c7e3ab6fdc7675233b20533
IgaGooo <igagooo777@yandex.ru>
2022-01-07 22:57:46 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
-$(function(){
+$(function () {
 
+    var mixer = mixitup('.products__inner-box');
 
-    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        ratedFill: "#ffa726",
+        readOnly: true
+    });
+
+    $(".product__slider-inner").slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+    });
+
 });
